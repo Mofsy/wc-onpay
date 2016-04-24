34bc7476a829653264ada2aade84d583bef31d55
Mofsy <vptuhrcOmlCPRhhK9k8M>
2016-04-24 07:27:36 +0300

    Initial commit

---
+++ wc-onpay.php
@@ -0,0 +1,20 @@
+<?php
+/*
+    Plugin Name: WooCommerce - OnPay Payment Gateway
+    Plugin URI: https://mofsy.ru/portfolio/woocommerce-onpay-payment-gateway.html
+    Description: Allows you to use OnPay payment gateway with the WooCommerce plugin.
+    Version: 0.1.0.1
+    Author: Mofsy
+    Author URI: https://mofsy.ru
+    Text Domain: wc-onpay
+    Domain Path: /languages
+
+    Copyright: © 2016 Mofsy.
+	License: GNU General Public License v3.0
+	License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+
+if(!defined('ABSPATH'))
+{
+    exit;
+}
